ee11291e1446fe7b3018a64f6332eeffd574488b
mugeylmz7 <mugeylmz1994@gmail.com>
2026-08-01 00:23:38 +0300

    export stripeApiKey for external access

--- src/common/stripe.ts
+++ src/common/stripe.ts
@@ -2,7 +2,7 @@ import stripeLibrary from 'stripe';
 import './env'
 
 export const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-const stripeApiKey = process.env.STRIPE_API_KEY || '';
+export const stripeApiKey = process.env.STRIPE_API_KEY || '';
 
 if (!endpointSecret || !stripeApiKey) {
   console.warn('Stripe keys are missing, check the setup and try again.')
