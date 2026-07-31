7dbb341839eb059211a496c0a90cb32aa8f587e9
mugeylmz7 <mugeylmz1994@gmail.com>
2026-08-01 00:44:48 +0300

    fix: update stripe.ts to handle missing Stripe keys with a warning

--- src/common/stripe.ts
+++ src/common/stripe.ts
@@ -1,12 +1,9 @@
-import stripeLibrary from 'stripe';
+import stripeLibrary from 'stripe'
 import './env';
 
 export const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-export const stripeApiKey = process.env.STRIPE_SECRET_KEY;
-
-
-if (!endpointSecret || !stripeApiKey) {
-  throw new Error('Stripe keys are missing, check the setup and try again.');
+const stripeApiKey = process.env.STRIPE_SECRET_KEY || '';
+if (!endpointSecret || !stripeApiKey ) {
+  console.warn('Stripe keys are missing, check the setup and try again.')
 }
-
 export const stripe = stripeLibrary(stripeApiKey);
