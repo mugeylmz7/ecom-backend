eab7fa987696abb44894826fdb7c86d4a91402d7
mugeylmz7 <mugeylmz1994@gmail.com>
2026-08-01 00:37:27 +0300

    fix: correct stripeApiKey environment variable reference

--- src/common/stripe.ts
+++ src/common/stripe.ts
@@ -2,7 +2,7 @@ import stripeLibrary from 'stripe';
 import './env';
 
 export const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-export const stripeApiKey = process.env.STRIPE_API_KEY;
+export const stripeApiKey = process.env.STRIPE_SECRET_KEY;
 
 
 if (!endpointSecret || !stripeApiKey) {
